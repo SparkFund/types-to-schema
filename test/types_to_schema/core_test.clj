5421a440beac460d11956075b8230e78b587e69c
Matthew Dunn <dunn.mat@husky.neu.edu>
2015-06-18 17:01:40 -0400

    adds tests for wrapping and unwrapping namespaces, wrapped functions going against their types, and basic `(schema type)` checks

--- test/types_to_schema/core_test.clj
+++ test/types_to_schema/core_test.clj
@@ -1,6 +1,38 @@
 (ns types-to-schema.core-test
   (:require [clojure.test :refer :all]
             [clojure.core.typed :as t]
+            [schema.core :as s]
             [types-to-schema.core :refer :all]
             [types-to-schema.test-functions :refer :all]
             [types-to-schema.unwrapped-functions :refer :all]))
+
+(use-fixtures :once (wrap-namespaces-fixture '[types-to-schema.test-functions]))
+
+(deftest test-schema
+  (is (= 3 (s/validate (schema Long) 3))
+      "tests that schema works on longs")
+  (is (= "Test" (s/validate (schema String) "Test"))
+      "tests that schema works for strings")
+  (is (= '{:test :testing-once} (s/validate (schema TestAlias) '{:test :testing-once}))
+      "tests that schema works for aliases")
+  (is (= (list (list nil)) (s/validate (schema TestRecursiveAlias) (list (list nil))))
+      "tests that recursive types work")
+  (is (= '{:test :testing-hmap} (s/validate (schema '{:test t/Keyword}) '{:test :testing-hmap})))
+  (is (= '["hey" 12] (s/validate (schema '[String Number]) '["hey" 12]))))
+
+(deftest test-wrapped-functions
+  (let [counter @function-calls]
+    (is (thrown? clojure.lang.ExceptionInfo (broken-input 5 6)))
+    (is (= counter @function-calls))
+    (is (thrown? clojure.lang.ExceptionInfo (broken-required-input "Break Please" 6)))
+    (is (= counter @function-calls))
+    (is (thrown? clojure.lang.ExceptionInfo (broken-output 1 6)))
+    (is (= (inc counter) @function-calls))))
+
+(deftest wrap-and-unwrap
+  (let [_ (wrap-namespaces! '[types-to-schema.unwrapped-functions])]
+    (is (thrown? clojure.lang.ExceptionInfo (ignores-arguments 5 6))
+        "tests that it won't allow you to use a function that goes against its type")
+    (is (do (unwrap-namespaces! '[types-to-schema.unwrapped-functions])
+            (= "This should break" (ignores-arguments 5 6)))
+        "tests that after unwrapping the function, you can now use this function")))
--- test/types_to_schema/test_functions.clj
+++ test/types_to_schema/test_functions.clj
@@ -5,9 +5,26 @@
 
 (t/defalias TestRecursiveAlias (t/U nil (t/List TestRecursiveAlias)))
 
+(def function-calls (atom 0))
+
 (t/ann ^:no-check broken-input [TestAlias Long -> Long])
 (defn broken-input
-  "Used where the function doesn't require a correct input but will not pass the schema checker"
+  "Used for when the function doesn't require a correct input but will not pass the schema checker"
   [a b]
+  (swap! function-calls inc)
   a)
+
+(t/ann ^:no-check broken-required-input [Long Long -> Long])
+(defn broken-required-input
+  "Used for when the function being used also requires a correct input"
+  [a b]
+  (swap! function-calls inc)
+  (+ a b))
+
+(t/ann ^:no-check broken-output [Long Long -> Number])
+(defn broken-output
+  "Used when the function gives an output different than what's being used in the type annotation"
+  [a b]
+  (swap! function-calls inc)
+  "Please break")
 
