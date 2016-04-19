e7e88359422417e3a6fe79f004727412f561762e
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2016-04-18 17:01:55 -0700

    v0.1.5 - Bump to using Typed Clojure 0.3.23

--- project.clj
+++ project.clj
@@ -1,10 +1,16 @@
-(defproject types-to-schema "0.1.4"
+(defproject types-to-schema "0.1.5"
   :description "Converts Typed Clojure types to Prismatic Schemas for runtime checking"
   :url "https://github.com/SparkFund/types-to-schema"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+
+                 [org.clojure/core.typed "0.3.23"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/tools.reader]]
+
+                 ;; pull in tools.reader manually as core.typed 0.3.23 asks for
+                 ;; conflicting 0.9.2 and 1.0.0-alpha1
                  [org.clojure/tools.reader "0.9.2"]
-                 [org.clojure/core.typed "0.3.14"
-                  :exclusions [org.clojure/tools.reader]]
+
                  [prismatic/schema "0.2.4"]])
